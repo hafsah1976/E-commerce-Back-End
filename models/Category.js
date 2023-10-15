120fabfe3ef410692e6b8f1176b1a5044056bc14
Khan-HB <hafsah999@gmail.com>
2023-10-15 04:55:31 -0400

    adding routes files, category-routes, product-routes, tag-routes

--- models/Category.js
+++ models/Category.js
@@ -1,8 +1,8 @@
-// Import necessary modules and dependencies
+// Importing the necessary modules and dependencies
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
-// Define the Category model class
+// Defining the Category model class
 class Category extends Model {}
 
 // Initialize the Category model with its properties
