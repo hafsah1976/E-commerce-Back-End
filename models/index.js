4fd89455822428cb0e63170e9db6453ca297d6e6
Khan-HB <hafsah999@gmail.com>
2023-10-15 19:34:10 -0400

    added comments to the model associations file

--- models/index.js
+++ models/index.js
@@ -4,26 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Define associations between models
+
 // Products belong to a Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+  foreignKey: 'category_id', // The foreign key in the Product model that associates it with Category
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
+  foreignKey: 'category_id', // The foreign key in the Product model that associates it with Category
 });
 
 // Products belong to many Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id',
+  through: ProductTag, // The intermediate model for the many-to-many relationship
+  foreignKey: 'product_id', // The foreign key in the ProductTag model that associates Product with Tag
 });
 
 // Tags belong to many Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id',
+  through: ProductTag, // The intermediate model for the many-to-many relationship
+  foreignKey: 'tag_id', // The foreign key in the ProductTag model that associates Tag with Product
 });
 
 module.exports = {
@@ -32,5 +34,3 @@ module.exports = {
   Tag,
   ProductTag,
 };
-
-
