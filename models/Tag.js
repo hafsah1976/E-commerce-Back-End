7220bfd0c4dc00421d97a24f0612bfddfb142925
Khan-HB <hafsah999@gmail.com>
2023-10-15 19:31:32 -0400

    added more comments for my understanding in the Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -7,30 +7,30 @@ const sequelize = require('../config/connection.js');
 // Initialize the Tag model by extending off Sequelize's Model class
 class Tag extends Model {}
 
-// Define and set up columns and rules for the Tag model
+// Defining and set up columns and rules for the Tag model
 Tag.init(
   {
-    // Define the id column
+    // Defining the id column
     id: {
       type: DataTypes.INTEGER,
-      primaryKey: true,       
-      allowNull: false,      
-      autoIncrement: true 
+      primaryKey: true,       // It's the primary key
+      allowNull: false,      // It cannot be null
+      autoIncrement: true   // It auto-increments
     },
-    // Define the tag_name column, which stores tag names
+    // Defining the tag_name column, which stores tag names
     tag_name: {
       type: DataTypes.STRING
     }
   },
   {
-    // Pass the sequelize instance for database connection
+    // Passing the sequelize instance for database connection
     sequelize,
-    timestamps: false,       
-    freezeTableName: true,   
-    underscored: true,       
-    modelName: 'tag',        
+    timestamps: false,       // Don't create createdAt and updatedAt columns
+    freezeTableName: true,   // Use the model name as the table name
+    underscored: true,       // Use snake_case for column names
+    modelName: 'tag',        // Set the model name to 'tag'
   }
 );
 
-// Export the Tag model 
+// Export the Tag model for use in other parts of the application
 module.exports = Tag;
