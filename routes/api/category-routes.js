dcc6c235d15d26713b33130464541c8dec8d8389
Khan-HB <hafsah999@gmail.com>
2023-10-15 05:04:36 -0400

    adding to category routes, route to get the category by ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,21 +2,53 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// Define a GET route for '/api/categories'
+// The `/api/categories` endpoint
 router.get('/', async (req, res) => {
   try {
-    // Find all categories from the Category model
+    // Find all categories
     const categoryData = await Category.findAll({
+      // Include associated Products
+      include: [{ model: Product }],
+    });
+
+    // Check if there are no categories found
+    if (categoryData.length === 0) {
+      return res.status(404).json({ message: 'No categories found.' });
+    }
+
+    // Send the category data as a JSON response
+    res.json(categoryData);
+  } catch (error) {
+    console.error(error);
+
+    // Send a 500 Internal Server Error response with an error message
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+  }
+});
+
+// Define a GET route for '/api/categories/:id' to retrieve a single category by ID
+router.get('/:id', async (req, res) => {
+  try {
+    // Find one category by its `id` value using the Category model
+    const categoryData = await Category.findByPk(req.params.id, {
       // Include associated Products in the result
       include: [{ model: Product }],
     });
 
+    // Check if the categoryData is null, meaning no category was found with the given ID
+    if (!categoryData) {
+      res.status(404).json({ message: 'Failed to find the requested Category.' });
+      return;
+    }
+
     // Send the category data as a JSON response
     res.json(categoryData);
-  } catch (err) {
+  } catch (error) {
     // If an error occurs during the process, log the error to the console
-    console.error(err);
-    // Send a 500 Internal Server Error response along with the error details as JSON
-    res.status(500).json(err);
+    console.error(error);
+    // Send a 500 Internal Server Error response with the error details as JSON
+    res.status(500).json(error);
   }
 });
+
+
